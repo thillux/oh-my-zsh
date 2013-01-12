24ff3c79271457c796c8ab8b679739ad1367a99e
thillux <theil.markus@gmail.com>
2013-01-12 14:02:46 +0100

    added section messages

--- pullFromRobbyRussell.sh
+++ pullFromRobbyRussell.sh
@@ -1,4 +1,13 @@
 #!/bin/zsh
 
+echo "Pull changes from Robby Russel's repository"
+echo "###########################################"
+echo ""
 git pull git@github.com:robbyrussell/oh-my-zsh.git
+
+echo ""
+
+echo "Push changes to own repository"
+echo "##############################"
+echo ""
 git push
