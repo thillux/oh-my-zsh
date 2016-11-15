33c804a9d444c51b63206917d058cf2c2d841e45
thillux <theil.markus@gmail.com>
2016-11-15 09:30:51 +0100

    import archlinux plugin again

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -1,24 +1,4 @@
 if (( $+commands[yaourt] )); then
-<<<<<<< HEAD
-  upgrade () {
-    yaourt -Syua
-  }
-  alias yaconf='yaourt -C'        # Fix all configuration files with vimdiff
-  # Pacman - https://wiki.archlinux.org/index.php/Pacman_Tips
-  alias yaupg='yaourt -Syua'        # Synchronize with repositories before upgrading packages (AUR packages too) that are out of date on the local system.
-  alias yasu='yaourt --sucre'      # Same as yaupg, but without confirmation
-  alias yain='yaourt -S'           # Install specific package(s) from the repositories
-  alias yains='yaourt -U'          # Install specific package not from the repositories but from a file
-  alias yare='yaourt -R'           # Remove the specified package(s), retaining its configuration(s) and required dependencies
-  alias yarem='yaourt -Rns'        # Remove the specified package(s), its configuration(s) and unneeded dependencies
-  alias yarep='yaourt -Si'         # Display information about a given package in the repositories
-  alias yareps='yaourt -Ss'        # Search for package(s) in the repositories
-  alias yaloc='yaourt -Qi'         # Display information about a given package in the local database
-  alias yalocs='yaourt -Qs'        # Search for package(s) in the local database
-  alias yalst='yaourt -Qe'         # List installed packages, even those installed from AUR (they're tagged as "local")
-  alias yaorph='yaourt -Qtd'       # Remove orphans using yaourt
-  # Additional yaourt alias examples
-=======
   alias yaconf='yaourt -C'
   alias yaupg='yaourt -Syua'
   alias yasu='yaourt -Syua --noconfirm'
@@ -63,7 +43,6 @@ if (( $+commands[pacaur] )); then
   alias painsd='pacaur -S --asdeps'
   alias pamir='pacaur -Syy'
 
->>>>>>> 0b340bc3a5c58609a07987b296f773eaea17b274
   if (( $+commands[abs] && $+commands[aur] )); then
     alias paupd='pacaur -Sy && sudo abs && sudo aur'
   elif (( $+commands[abs] )); then
